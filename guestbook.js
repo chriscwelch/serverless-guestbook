ebb70b25e80b5e8ae916b3d11a0cd858add34b79
chriscwelch <82319644+chriscwelch@users.noreply.github.com>
2021-08-17 21:10:34 +0100

    api url update

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://29630f93.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
